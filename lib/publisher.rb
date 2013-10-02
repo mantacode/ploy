b93bfdb90ce9eb1e19cea52f877ff8f589687857
Michael Bruce <bruce@jhereg.net>
2013-10-02 14:58:03 -0400

    Updates, updates.

--- lib/publisher.rb
+++ lib/publisher.rb
@@ -2,25 +2,59 @@ require 'yaml'
 
 module Ploy
   class Publisher
+    def initialize(conf_source)
+          @conf = conf_source
+          if (/^---/ =~ conf_source) then
+            @conf = YAML::load(conf_source)
+          else
+            @conf = YAML::load_file(conf_source)
+          end
+    end
 
-    class << self
+    def publish()
+      self.prep()
+    end
 
-      def publish(conf_source)
-        conf = process_conf(conf_source)
+    def prep()
+      if (@conf['prep_cmd']) then
+        system(@conf['prep_cmd'])
+      end
+    end
 
-        system(conf['prep_cmd'])
+    def package()
+      opts = [
+        { "-n" => @conf['deploy_name'] },
+        { "-s" => "dir" },
+        { "-t" => "deb" },
+        @conf['dist_dir']
+      ]
+
+      opts_string = stringify_optlist(opts)
+      cmd = "fpm #{opts_string}"
+      info = eval(`#{cmd}`) # yes, really
+
+      return info[:path]
+    end
+
+    def send()
+
+    end
+
+    private
+      def stringify_optlist(opts)
+        opts_flat = []
+
+        opts.each do | e |
+          if (e.is_a? Hash) then
+            e.each { | k, v| opts_flat.push([k,v].join(' ')) }
+          else
+            opts_flat.push(e)
+          end
+        end
+
+        return opts_flat.join(' ')
       end
 
-      private
-        def process_conf(conf_source)
-          conf = conf_source
-          if (/^---/ =~ conf_source) then
-            conf = YAML::load(conf_source)
-          else
-            conf = YAML::load_file(conf_source)
-          end
-          return conf
-        end
-    end
   end
+
 end
--- spec/publisher_spec.rb
+++ spec/publisher_spec.rb
@@ -1,9 +1,36 @@
 require './lib/publisher'
 
 describe Ploy::Publisher do
-  it "runs a prep command before publishing" do
-    Ploy::Publisher.should_receive("system").with("lineman build")
-    Ploy::Publisher.publish("spec/resources/ploy-publish.yml")
+  before(:all) do
+    @pub = Ploy::Publisher.new("spec/resources/ploy-publish.yml")
   end
+
+  it "is initialized correctly" do
+    expect(@pub).to be_a(Ploy::Publisher)
+  end
+
+  it "runs prep steps correctly" do
+    @pub.should_receive("system").with("lineman build")
+    @pub.prep()
+  end
+
+  describe "#package" do
+    filename = ""
+
+    it "builds a deb file" do
+      path = @pub.package()
+      expect(File.exists? path).to be_true
+      filename = path
+    end
+
+    it "sets a version that looks right" do
+    end
+
+    after(:all) do
+      File.delete(filename)
+    end
+
+  end
+
 end
 
