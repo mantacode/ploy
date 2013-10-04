25a2c5a9a41a0fbb4e4dbdc2199320350d7f1823
Michael Bruce <bruce@jhereg.net>
2013-10-03 23:00:42 -0400

    Add minimal help output.

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,8 +4,8 @@ require 'installer'
 module Ploy
   class Cli
     def run(argv)
-      subcommand = argv.shift
-       # obvs a serious of if statements is lame. refactor later.
+      subcommand = argv.shift || 'help'
+      # obvs a serious of if statements is lame. refactor later.
       if (subcommand == "publish") then
         filename = argv.shift
         if (not filename) then
@@ -15,6 +15,16 @@ module Ploy
       elsif (subcommand == "install") then
         bucket, deploy, branch, version = argv
         Ploy::Installer.install(bucket, deploy, branch, version)
+      elsif (subcommand == "help") then
+        text = <<-helptext
+usage: ploy [command] [options]
+
+Commands:
+  publish         Package the current git repository and send to S3
+  install         Pull a deployment from S3 and install on current system
+  help            Show help
+        helptext
+        puts text
       end
     end
   end
