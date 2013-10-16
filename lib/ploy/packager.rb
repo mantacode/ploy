3c856cc92dc4380ffea2df83c515e41734682bb8
Michael Bruce <mbruce@manta.com>
2013-10-16 10:24:05 -0400

    checkpoint

--- lib/ploy/packager.rb
+++ lib/ploy/packager.rb
@@ -11,7 +11,18 @@ module Ploy
     
     def build
       prep
-      pkgpath = self.build_deb
+      builder = Ploy::DebBuilder.new(
+        :name          => @conf['deploy_name'],
+        :sha           => git_revision,
+        :branch        => git_branch,
+        :timestamp     => git_timestamp,
+        :upstart_files => @conf['upstart_files'],
+        :dist_dir      => @conf['dist_dir'],
+        :prefix        => @conf['prefix']
+      );
+
+      pkgpath = builder.build_deb
+
       return pkgpath
     end
 
@@ -21,39 +32,6 @@ module Ploy
       end
     end
 
-    def build_deb
-      info = nil
-      Dir.mktmpdir do |dir|
-        mirror_dist(dir)
-
-        opts = [
-          { "-n" => @conf['deploy_name'] },
-          { "-s" => "dir" },
-          { "-t" => "deb" },
-          { "-a" => "all" },
-          { "-C" => dir },
-          { "--deb-field" => "'gitrev: #{git_revision}'" },
-          "-f",
-          { "-v" => git_timestamp + '.' + git_branch },
-          "."
-        ]
-
-        if (@conf['upstart_files']) then
-          @conf['upstart_files'].each do | upstart |
-            opts.unshift({ "--deb-upstart" => upstart })
-          end
-        end
-
-        write_metadata(dir)
-
-        opts_string = stringify_optlist(opts)
-        cmd = "fpm #{opts_string}"
-        info = eval(`#{cmd}`) # yes, really
-
-      end
-
-      return info[:path]
-    end
 
     def remote_package
       return Ploy::Package.new(
@@ -65,43 +43,6 @@ module Ploy
     end
 
     private
-    def stringify_optlist(opts)
-      opts_flat = []
-
-      opts.each do | e |
-        if (e.is_a? Hash) then
-          e.each { | k, v| opts_flat.push([k,v].join(' ')) }
-        else
-          opts_flat.push(e)
-        end
-      end
-
-      return opts_flat.join(' ')
-    end
-
-    def mirror_dist(dir)
-      dist_target = dir
-      if (@conf['prefix']) then
-        dist_target = File.join(dist_target, @conf['prefix'])
-      end
-      FileUtils.mkpath dist_target
-      system("rsync -aC #{@conf['dist_dir']}/* #{dist_target}")
-    end
-
-    def write_metadata(dir)
-      base = File.join(dir, "/etc/ploy/metadata.d")
-      FileUtils.mkpath(base)
-      path = File.join(base, "#{@conf['deploy_name']}.yml")
-      info = {
-        "name"      => @conf['deploy_name'],
-        "sha"       => git_revision,
-        "timestamp" => git_timestamp,
-        "branch"    => git_branch
-      }
-      File.open(path, 'w') do | out |
-        YAML.dump(info, out)
-      end
-    end
 
     def git_branch
       return ENV['TRAVIS_BRANCH'] || `git symbolic-ref --short -q HEAD`.chomp
