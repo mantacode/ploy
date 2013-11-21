8ebfd25af88f9d3d70d8e566f635d314ef4b550b
Stephen J. Smith <stsmith@manta.com>
2013-11-21 14:15:08 -0500

    janitorial: copy dist_dirs by value for maintainability

--- lib/ploy/localpackage/debbuilder.rb
+++ lib/ploy/localpackage/debbuilder.rb
@@ -22,11 +22,11 @@ module Ploy
       def build_deb
         info = nil
         Dir.mktmpdir do |dir|
-          dist_dirs = @dist_dirs || []
+          dist_dirs = Array.new(@dist_dirs || [])
           if @dist_dir
             dist_dirs.unshift({'dir' => @dist_dir, 'prefix' => @prefix})
           end
-          mirror_dists(dir, @dist_dirs)
+          mirror_dists(dir, dist_dirs)
           #mirror_dist(dir, @prefix, @dist_dir)
           write_metadata(dir)
           Tempfile.open(['postinst', 'sh']) do |file|
