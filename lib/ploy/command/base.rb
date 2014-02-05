67d042d00b7ce47e87d9132df1142a80f208a0ca
NathanGRomano <nathan.g.romano@gmail.com>
2014-02-05 13:23:29 -0500

    added a method to set the argv that will be passed to the run method

--- lib/ploy/command/base.rb
+++ lib/ploy/command/base.rb
@@ -4,9 +4,27 @@ module Ploy
     # A base class for commands
 
     class Base
+
+      # sets/gets the argv 
+
+      def argv (v=nil)
+        @argv = @argv || v || []
+        if v
+          return self
+        else
+          return @argv
+        end
+      end
+
+      # execute the run command and tries to execute the next command
+      
+      def execute (argv=[])
+        run(argv)
+      end
+
       # run with these arguments
 
-      def run(argv)
+      def run(argv=[])
         return false
       end
 
@@ -15,6 +33,17 @@ module Ploy
       def help
         return ""
       end
+
+      # the next command
+      
+      def next (command=nil)
+        if command.is_a?(Base)
+          @next = command
+          return self
+        end
+        return @next
+      end
+
     end
   end
 end
