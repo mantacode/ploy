c05576a7ca6de2574e93336b8aac51b6ba19ed07
NathanGRomano <nathan.g.romano@gmail.com>
2014-02-06 02:35:53 -0500

    modified the cli to lookup up our command attach the hooks command next and then execute the chain of commands

--- lib/ploy/cli.rb
+++ lib/ploy/cli.rb
@@ -10,10 +10,8 @@ module Ploy
   class Cli
     def run(argv)
       name = argv.shift || 'help'
-      scripts = File.basename(Dir.getwd) + '/events.d'
-      puts 'the scripts: ' + scripts
+      scripts = File.basename(Dir.getwd) + '/events.d/' + name
       Ploy::Command.lookup(name).next(Ploy::Command::Hooks.new(scripts)).execute(argv);
-      Ploy::Command.lookup(subcommand).run(argv)
     end
   end
 end
