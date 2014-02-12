e3a3e183d046553f31b0ff03b9e15d543154f2ba
Nathan G. Romano <nathan.g.romano@gmail.com>
2014-02-12 14:36:17 -0500

    Update metasrc.rb
    remove comments

--- lib/ploy/metasrc.rb
+++ lib/ploy/metasrc.rb
@@ -1,19 +1,9 @@
 module Ploy
-
-  # Loads Meta data from YAML files
-
   class MetaSrc
-
-    # initialize an instance with the directory specified
 
     def initialize(dir)
       @dir = dir
     end
-
-    # Uses the instance's dir attribute to load the yaml
-    # files in that directory.  The yaml data is stored
-    # into a hash using the "name" attribute from the yaml
-    # document
 
     def load
       d = {}
